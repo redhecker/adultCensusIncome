ad3164fc1ccb5c7351264550534b5b5ba693604f
redhecker <12210305@mail.sustech.edu.cn>
2024-05-15 20:50:10 +0800

    add visualization

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello")
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+data = pd.read_csv('..\\data\\traindata.csv')
+labels = pd.read_csv('..\\data\\trainlabel.txt', header=None, names=['label'])['label']
+data.insert(0, 'income', labels)
