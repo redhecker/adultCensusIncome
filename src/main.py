60319bd352a8c2c1741fba92f8d8e6422273cdc4
redhecker <12210305@mail.sustech.edu.cn>
2024-06-09 13:17:13 +0800

    update data processing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,1 @@
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
 
-data = pd.read_csv('..\\data\\traindata.csv')
-labels = pd.read_csv('..\\data\\trainlabel.txt', header=None, names=['label'])['label']
-data.insert(0, 'income', labels)
