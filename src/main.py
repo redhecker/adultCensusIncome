735c3cc6e2cf4f3a8d2e2cf9eb253ec44e4f8e6a
redhecker <12210305@mail.sustech.edu.cn>
2024-06-09 16:45:12 +0800

    add test result

--- src/main.py
+++ src/main.py
@@ -24,53 +24,55 @@ for col in num_col:
     encode_data_test[col] = (encode_data_test[col] - encode_data_test[col].mean()) / encode_data_test[col].std()
 
 # make the col to the same
-for col in encode_data.columns:
+for col in encode_data_test.columns:
     if col not in encode_data.columns:
         encode_data_test.drop(col, axis=1, inplace=True)
 
-modelChose = sys.argv[2]
-
-
-if modelChose == 'help' or modelChose == 'Help' or modelChose == '--help' or modelChose == '-h':
-    print('Usage: python main.py [model]')
-    print('model: [RD, DT, SGD, LR, RF, GB]')
-    sys.exit(0)
-elif modelChose == 'RD':
-    print('Random')
-    pred_label = np.zeros(encode_data_test.shape[0])
-    np.savetxt('..\\predResult\\Random.txt', pred_label, fmt='%d')
-elif modelChose == 'SGD':
-    print('Stochastic Gradient Descent')
-    model = SGDClassifier()
-    model.fit(encode_data.drop('income', axis=1), encode_data['income'])
-    pred_label = model.predict(encode_data_test)
-    np.savetxt('..\\predResult\\SGD.txt', pred_label, fmt='%d')
-elif modelChose == 'DT':
-    print('Decision Tree')
-    model = DecisionTreeClassifier()
-    model.fit(encode_data.drop('income', axis=1), encode_data['income'])
-    pred_label = model.predict(encode_data_test)
-    np.savetxt('..\\predResult\\DecisionTree.txt', pred_label, fmt='%d')
-elif modelChose == 'LR':
-    print('Logistic Regression')
-    model = LogisticRegression()
-    model.fit(encode_data.drop('income', axis=1), encode_data['income'])
-    pred_label = model.predict(encode_data_test)
-    np.savetxt('..\\predResult\\LogisticRegression.txt', pred_label, fmt='%d')
-elif modelChose == 'RF':
-    print('Random Forest')
-    model = RandomForestClassifier()
-    model.fit(encode_data.drop('income', axis=1), encode_data['income'])
-    pred_label = model.predict(encode_data_test)
-    np.savetxt('..\\predResult\\RandomForest.txt', pred_label, fmt='%d')
-elif modelChose == 'GB':
-    print('Gradient Boosting')
-    model = GradientBoostingClassifier()
-    model.fit(encode_data.drop('income', axis=1), encode_data['income'])
-    pred_label = model.predict(encode_data_test)
-    np.savetxt('..\\predResult\\GradientBoosting.txt', pred_label, fmt='%d')
-else:
-    print('No such model: ', modelChose)
-    print('Usage: python main.py [model]')
-    print('model: [RD, DT, SGD, LR, RF, GB]')
-    sys.exit(0)
+while True:
+    print("input th model you would like to use: (print help to get help and print exit to exit)")
+    print(">>", end='')
+    modelChose = input()
+    if modelChose == 'help' or modelChose == 'Help' or modelChose == '--help' or modelChose == '-h':
+        print('Usage: python main.py [model]')
+        print('model: [RD, DT, SGD, LR, RF, GB]')
+    elif modelChose == 'RD':
+        print('Random')
+        pred_label = np.zeros(encode_data_test.shape[0])
+        np.savetxt('..\\predResult\\Random.txt', pred_label, fmt='%d')
+    elif modelChose == 'SGD':
+        print('Stochastic Gradient Descent')
+        model = SGDClassifier()
+        model.fit(encode_data.drop('income', axis=1), encode_data['income'])
+        pred_label = model.predict(encode_data_test)
+        np.savetxt('..\\predResult\\SGD.txt', pred_label, fmt='%d')
+    elif modelChose == 'DT':
+        print('Decision Tree')
+        model = DecisionTreeClassifier()
+        model.fit(encode_data.drop('income', axis=1), encode_data['income'])
+        pred_label = model.predict(encode_data_test)
+        np.savetxt('..\\predResult\\DecisionTree.txt', pred_label, fmt='%d')
+    elif modelChose == 'LR':
+        print('Logistic Regression')
+        model = LogisticRegression()
+        model.fit(encode_data.drop('income', axis=1), encode_data['income'])
+        pred_label = model.predict(encode_data_test)
+        np.savetxt('..\\predResult\\LogisticRegression.txt', pred_label, fmt='%d')
+    elif modelChose == 'RF':
+        print('Random Forest')
+        model = RandomForestClassifier()
+        model.fit(encode_data.drop('income', axis=1), encode_data['income'])
+        pred_label = model.predict(encode_data_test)
+        np.savetxt('..\\predResult\\RandomForest.txt', pred_label, fmt='%d')
+    elif modelChose == 'GB':
+        print('Gradient Boosting')
+        model = GradientBoostingClassifier()
+        model.fit(encode_data.drop('income', axis=1), encode_data['income'])
+        pred_label = model.predict(encode_data_test)
+        np.savetxt('..\\predResult\\GradientBoosting.txt', pred_label, fmt='%d')
+    elif modelChose == 'exit' or modelChose == 'quit' or modelChose == 'q':
+        print('exiting')
+        sys.exit(0)
+    else:
+        print('No such model: ', modelChose)
+        print('Usage: python main.py [model]')
+        print('model: [RD, DT, SGD, LR, RF, GB]')
